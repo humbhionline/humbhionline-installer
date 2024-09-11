d8f0895b404bff78d7d25ac5c1867e5a590110fd
Venky <venkatramanm@gmail.com>
2024-09-11 21:45:34 +0530

    Fixes

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 tooldir=`dirname $0`
 tooldir=$PWD/$tooldir
 application_dir="$tooldir/application"
-humbionline_repo=git@github.com:venkatramanm/humbhionline.git
+humbionline_repo=git@github.com:humbhionline/humbhionline.git
 
 mkdir -p $application_dir
 cd $application_dir
 
-for repo in git@github.com:venkatramanm/common.git git@github.com:venkatramanm/swf-all.git git@github.com:venkatramanm/swf-plugin-mobilesignup.git git@github.com:venkatramanm/swf-plugin-slideshow.git git@github.com:venkatramanm/swf-plugin-calendar.git git@github.com:venkatramanm/swf-plugin-wiki.git git@github.com:venkatramanm/swf-plugin-ecommerce.git	git@github.com:venkatramanm/beckn-sdk-java.git $humbionline_repo
+for repo in git@github.com:open-succinct-community/common.git git@github.com:open-succinct-community/reflection.git git@github.com:open-succinct-community/swf-all.git git@github.com:open-succinct-community/reflection.git git@github.com:open-succinct-community/swf-plugin-mobilesignup.git git@github.com:open-succinct-community/swf-plugin-slideshow.git git@github.com:open-succinct-community/swf-plugin-calendar.git git@github.com:open-succinct-community/swf-plugin-wiki.git git@github.com:open-succinct-community/swf-plugin-ecommerce.git git@github.com:beckn-on-succinct/beckn-sdk-java.git git@github.com:beckn-on-succinct/swf-plugin-beckn.git git@github.com:beckn-on-succinct/onet.core git@github.com:beckn-on-succinct/onet.boc git@github.com:beckn-on-succinct/bap.shell $humbionline_repo
 do 
     echo "Building $repo"
     dir=`basename $repo |sed 's/.git//g'`
